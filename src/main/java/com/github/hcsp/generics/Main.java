21f5f29a2cb43084975d15a1e791bd3fcfb1a45e
guakun <maguahongkun@outlook.com>
2019-09-25 16:31:33 +0800

    make method generic

--- src/main/java/com/github/hcsp/generics/Main.java
+++ src/main/java/com/github/hcsp/generics/Main.java
@@ -4,21 +4,13 @@ public class Main {
     // 这里有四个结构、功能非常相似的方法，请尝试将其范型化，以简化代码
     // 范型化之后的方法签名应该如下所示：
     // public static boolean inAscOrder(T a, T b, T c)
-    public static boolean inAscOrder1(int a, int b, int c) {
-        return a <= b && b <= c;
-    }
-
-    public static boolean inAscOrder2(long a, long b, long c) {
-        return a <= b && b <= c;
-    }
-
-    public static boolean inAscOrder3(double a, double b, double c) {
-        return a <= b && b <= c;
+    public static <T extends Comparable> boolean inAscOrder(T a, T b, T c) {
+        return a.compareTo(b) <= 0 && b.compareTo(c) <= 0;
     }
 
     public static void main(String[] args) {
-        System.out.println(inAscOrder1(1, 2, 3));
-        System.out.println(inAscOrder2(1L, 2L, 3L));
-        System.out.println(inAscOrder3(1d, 2d, 3d));
+        System.out.println(inAscOrder(1, 2, 3));
+        System.out.println(inAscOrder(1L, 2L, 3L));
+        System.out.println(inAscOrder(1d, 2d, 3d));
     }
 }
