68f87d9e731d541e71e00d9a09f46fe706409886
Ryan <49991585+Ryan-Y-C@users.noreply.github.com>
2020-08-31 10:58:43 +0800

    实现泛型化的方法 (#91)
    实现泛型化的方法 (#91)

--- src/main/java/com/github/hcsp/generics/Main.java
+++ src/main/java/com/github/hcsp/generics/Main.java
@@ -1,19 +1,25 @@
 package com.github.hcsp.generics;
 
+
 public class Main {
     // 这里有四个结构、功能非常相似的方法，请尝试将其泛型化，以简化代码
     // 泛型化之后的方法签名应该如下所示：
     // public static boolean inAscOrder(T a, T b, T c)
+
+    public static <T extends Comparable<T>> boolean inAscOrder(T a, T b, T c) {
+        return a.compareTo(b) <= 0 && b.compareTo(c) <= 0;
+    }
+
     public static boolean inAscOrder1(int a, int b, int c) {
-        return a <= b && b <= c;
+        return Main.inAscOrder(a, b, c);
     }
 
     public static boolean inAscOrder2(long a, long b, long c) {
-        return a <= b && b <= c;
+        return Main.inAscOrder(a, b, c);
     }
 
     public static boolean inAscOrder3(double a, double b, double c) {
-        return a <= b && b <= c;
+        return Main.inAscOrder(a, b, c);
     }
 
     public static void main(String[] args) {
