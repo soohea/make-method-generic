5a665d4d6f013d5bba64c32c31a587846efe899a
iRuby <1479867360@qq.com>
2020-12-29 18:12:30 +0800

    编写泛型化的方法

--- src/main/java/com/github/hcsp/generics/Main.java
+++ src/main/java/com/github/hcsp/generics/Main.java
@@ -3,7 +3,12 @@ package com.github.hcsp.generics;
 public class Main {
     // 这里有四个结构、功能非常相似的方法，请尝试将其泛型化，以简化代码
     // 泛型化之后的方法签名应该如下所示：
-    // public static boolean inAscOrder(T a, T b, T c)
+    public static <T extends Comparable<T>> boolean inAscOrder(T a, T b, T c) {
+        int i = b.compareTo(a);
+        int j = c.compareTo(b);
+        return (i >= 0) && (j >= 0);
+    }
+
     public static boolean inAscOrder1(int a, int b, int c) {
         return a <= b && b <= c;
     }
