04de9af0e0c019fdfb86ad3680ab0f31b6ef0ca4
wizard <42760426+qipengwei@users.noreply.github.com>
2021-01-08 22:22:40 +0800

    泛型 抽取公共方法 (#104)
    泛型 抽取公共方法 (#104)

--- src/main/java/com/github/hcsp/generics/Main.java
+++ src/main/java/com/github/hcsp/generics/Main.java
@@ -3,7 +3,9 @@ package com.github.hcsp.generics;
 public class Main {
     // 这里有四个结构、功能非常相似的方法，请尝试将其泛型化，以简化代码
     // 泛型化之后的方法签名应该如下所示：
-    // public static boolean inAscOrder(T a, T b, T c)
+     public static <T extends Comparable> boolean inAscOrder(T a, T b, T c) {
+        return a.compareTo(b) <= 0 && b.compareTo(c) <= 0;
+     }
     public static boolean inAscOrder1(int a, int b, int c) {
         return a <= b && b <= c;
     }
@@ -17,8 +19,8 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println(inAscOrder1(1, 2, 3));
-        System.out.println(inAscOrder2(1L, 2L, 3L));
-        System.out.println(inAscOrder3(1d, 2d, 3d));
+        System.out.println(inAscOrder(1, 2, 3));
+        System.out.println(inAscOrder(1L, 2L, 3L));
+        System.out.println(inAscOrder(1d, 2d, 3d));
     }
 }
